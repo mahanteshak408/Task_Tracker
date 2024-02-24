5a3e2a8c5160d0cd8b8f475f836690df8a9b9bf2
mahanteshaCloudMPower <mahantesha@cloudmpower.com>
2024-02-24 10:23:32 +0530

    changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
-import HelloWorld from '../components/HelloWorld.vue'
+import TaskList from '../components/TaskList.vue'
 
 
 </script>
@@ -10,7 +10,7 @@ import HelloWorld from '../components/HelloWorld.vue'
 
     
     <div class=" my-5 mx-10 w-1/2 text-white dark:bg-black dark:text-black">
-      <HelloWorld />
+      <TaskList />
     </div>
   
   </main>
